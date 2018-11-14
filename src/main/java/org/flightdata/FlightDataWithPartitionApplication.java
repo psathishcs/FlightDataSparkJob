ba9c000987494021a3d76ceb528c2f384365bb00
Sathish Kumar Parthasarathy <p.sathish.cs@gmail.com>
2018-11-14 18:31:59 -0500

    write to hive table with partition

---
+++ src/main/java/org/flightdata/FlightDataWithPartitionApplication.java
@@ -0,0 +1,30 @@
+package org.flightdata;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.SparkSession;
+
+public class FlightDataWithPartitionApplication {
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("No files provided");
+			System.exit(0);
+		}
+		processData(args[0]);
+	}
+	
+	private static void processData(String fileName) {
+		System.out.println("fileName : " + fileName);
+		SparkSession spark =  SparkSession
+				.builder()
+				.appName("FlightDataJob")
+				.config("spark.master", "local")
+				.config("hive.metastore.uris", "thrift://skylark.datalake:9083")
+				.enableHiveSupport()
+				.getOrCreate();
+		Dataset  inputFile =  spark.read().option("header", "true").csv("hdfs://skylark.datalake:9000/"+fileName);
+		inputFile.show();
+		System.out.println("inputFile.count() : " + inputFile.count()); 
+		inputFile.write().mode(SaveMode.Overwrite).saveAsTable("datasets.flight_data");
+	}
+}
