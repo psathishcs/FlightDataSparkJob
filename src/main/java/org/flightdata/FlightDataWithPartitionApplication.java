3c512138538ab22eba1cbe1f423ee31ab3b64153
Sathish Kumar Parthasarathy <p.sathish.cs@gmail.com>
2018-11-14 19:09:59 -0500

    adding partitionBy

--- src/main/java/org/flightdata/FlightDataWithPartitionApplication.java
+++ src/main/java/org/flightdata/FlightDataWithPartitionApplication.java
@@ -25,6 +25,6 @@ public class FlightDataWithPartitionApplication {
 		Dataset  inputFile =  spark.read().option("header", "true").csv("hdfs://skylark.datalake:9000/"+fileName);
 		inputFile.show();
 		System.out.println("inputFile.count() : " + inputFile.count()); 
-		inputFile.write().mode(SaveMode.Overwrite).saveAsTable("flight.flight_data_partition_by_month");
+		inputFile.write().mode(SaveMode.Overwrite).partitionBy("month").saveAsTable("flight.flight_data_partition_by_month");
 	}
 }
